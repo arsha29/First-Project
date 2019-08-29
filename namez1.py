75df68234f6aff3cc8e3e6d1217d496626541ba2
arsha29 <54542106+arsha29@users.noreply.github.com>
2019-08-28 20:09:50 -0400

    name printer

---
+++ namez1.py
@@ -0,0 +1,11 @@
+namez = input("What is your name?: ")
+
+
+def salutations(namez):
+    print("Good Morning " + namez + ", what would you like to do today?")
+   
+      
+
+            
+
+salutations(namez)
